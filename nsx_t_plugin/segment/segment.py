7571a07a3fdbf858557efbc75a01ab7aa62f13ed
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-08-13 08:35:34 +0300

    CZ-85 Update handling the subnets for segment

--- nsx_t_plugin/segment/segment.py
+++ nsx_t_plugin/segment/segment.py
@@ -13,12 +13,32 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+from cloudify.exceptions import NonRecoverableError
+
 from nsx_t_plugin.decorators import with_nsx_t_client
 from nsx_t_sdk.resources import Segment
 
 
+def _update_subnet_configuration(resource_config):
+    subnet = resource_config.get('subnet')
+    if subnet:
+        dhcp_options_config = ['dhcp_v4_config', 'dhcp_v6_config']
+        if all([item in subnet for item in dhcp_options_config]):
+            raise NonRecoverableError(
+                'Both {0} are provided, only one of '
+                'them is allowed'.format(','.join(dhcp_options_config)))
+        dhcp_config = \
+            subnet.get('dhcp_v4_config') or subnet.get('dhcp_v6_config')
+        resource_config['subnet']['dhcp_config'] = dhcp_config
+        resource_config['subnets'] = []
+        resource_config['subnets'][0] = resource_config.pop('subnet')
+
+
 @with_nsx_t_client(Segment)
 def create(nsx_t_resource):
+    # Update the subnet configuration for segment
+    _update_subnet_configuration(nsx_t_resource.resource_config)
+    # Trigger the actual call to the NSXT Manager API
     resource = nsx_t_resource.create()
     # Update the resource_id with the new "id" returned from API
     nsx_t_resource.resource_id = resource.id
