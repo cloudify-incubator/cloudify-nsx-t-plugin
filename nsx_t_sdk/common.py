7cc58a0fbaf429245ea940462f3fdaa30c612b0c
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-08-16 17:53:10 +0300

    CZ-85 Update Segment node

--- nsx_t_sdk/common.py
+++ nsx_t_sdk/common.py
@@ -54,6 +54,7 @@ class NSXTResource(object):
         self.client_config = client_config
         self.logger = logger
         self.resource_config = resource_config or {}
+        self.resource_config['resource_type'] = self.resource_type
         self.resource_id = self.resource_config.pop('id')
         self._api_client = self._prepare_nsx_t_client()
 
