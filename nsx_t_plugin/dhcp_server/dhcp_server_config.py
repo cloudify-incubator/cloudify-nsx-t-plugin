8cc028f48822f79864c4cbea98f2f709bbed088d
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-08-19 12:16:36 +0300

    CZ-95 Handle dhcp deletion when it has a connection to tier 1 gateway

--- nsx_t_plugin/dhcp_server/dhcp_server_config.py
+++ nsx_t_plugin/dhcp_server/dhcp_server_config.py
@@ -19,6 +19,35 @@ from nsx_t_plugin.decorators import with_nsx_t_client
 from nsx_t_sdk.resources import DhcpServerConfig, Tier1
 
 
+def _update_tier_1_gateway(client_config, tier1_gateway_id, dhcp_server_paths):
+    tier1 = Tier1(
+        client_config=client_config,
+        resource_config={
+            'id': tier1_gateway_id
+        },
+        logger=ctx.logger
+    )
+    tier1_object = tier1.get()
+    tier1_object.dhcp_config_paths = dhcp_server_paths
+    tier1.patch(tier1_object)
+
+
+def _link_dhcp_server_to_tier_1(client_config, tier1_gateway_id):
+    _update_tier_1_gateway(
+        client_config,
+        tier1_gateway_id,
+        [ctx.instance.runtime_properties['path']]
+    )
+
+
+def _unlink_dhcp_server_from_tier_1(client_config, tier1_gateway_id):
+    _update_tier_1_gateway(
+        client_config,
+        tier1_gateway_id,
+        []
+    )
+
+
 @with_nsx_t_client(DhcpServerConfig)
 def create(nsx_t_resource):
     resource = nsx_t_resource.create()
@@ -34,18 +63,24 @@ def create(nsx_t_resource):
 def configure(nsx_t_resource):
     tier1_gateway_id = ctx.node.properties.get('tier1_gateway_id')
     if tier1_gateway_id:
-        tier1 = Tier1(
-            client_config=nsx_t_resource.nsx_t_resource,
-            resource_config={
-                'id': tier1_gateway_id
-            },
-            logger=ctx.logger
+        _link_dhcp_server_to_tier_1(
+            nsx_t_resource.client_config,
+            tier1_gateway_id
         )
-        tier1_object = tier1.get()
-        tier1_object.dhcp_config_paths = [
-            ctx.instance.runtime_properties['path']
-        ]
-        tier1.patch(tier1_object)
+    else:
+        ctx.logger.debug('No Tier1 gateway to attach DHCP Server to')
+
+
+@with_nsx_t_client(DhcpServerConfig)
+def stop(nsx_t_resource):
+    tier1_gateway_id = ctx.node.properties.get('tier1_gateway_id')
+    if tier1_gateway_id:
+        _unlink_dhcp_server_from_tier_1(
+            nsx_t_resource.client_config,
+            tier1_gateway_id
+        )
+    else:
+        ctx.logger.debug('No Tier1 gateway to detach DHCP Server from')
 
 
 @with_nsx_t_client(DhcpServerConfig)
